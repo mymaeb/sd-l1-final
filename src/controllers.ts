9272b8ede882a5c96054adc7e35d8f98e6d6b495
mymaeb <mymaeb@users.noreply.github.com>
2025-06-30 10:01:05 -0700

    Complete controller.ts

--- src/controllers.ts
+++ src/controllers.ts
@@ -1,4 +1,5 @@
 import { PelisCollection, Peli } from "./models";
+import * as jsonfile from "jsonfile";
 
 type Options = {
   id?: number;
@@ -9,21 +10,41 @@ type Options = {
 };
 
 class PelisController {
-
   model: PelisCollection;
-  
+
   constructor() {
     this.model = new PelisCollection();
   }
 
-  get(options?:Options):Peli[] {
+  async get(options?: Options): Promise<Peli[]> {
     if (options?.id) {
-      return this.model.getById(options.id);
+      const peli = await this.model.getById(options.id);
+      return peli ? [peli] : [];
     } else if (options?.search) {
-      return this.model.search(options.search);
+      return await this.model.search(options.search);
     } else {
-      return this.model.getAll();
+      return await this.model.getAll();
     }
   }
+
+  async getOne(options: Options): Promise<Peli | null> {
+    const resultados = await this.get(options);
+    return resultados.length > 0 ? resultados[0] : null;
+  }
+
+  async add(peli: Peli): Promise<boolean> {
+    const peliExistente = await this.model.getById(peli.id);
+
+    if (peliExistente) {
+      return false; 
+    }
+
+    const pelisActuales = await this.model.getAll();
+    pelisActuales.push(peli);
+
+    await jsonfile.writeFile("./pelis.json", pelisActuales);
+    return true;
+  }
 }
+
 export { PelisController };
